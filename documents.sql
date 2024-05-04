8e159064ded972ec8d48035c99d2889d86e534e9
tarunminator <48268681+tarunminator@users.noreply.github.com>
2024-05-04 13:25:18 -0400

    Update documents.sql

--- documents.sql
+++ documents.sql
@@ -1,5 +1,21 @@
-create table trip_docs (
-  id bigserial primary key,
-  content text, 
-  embedding vector(1024)
-);
+create or replace function match_trip_docs (
+    query_embedding vector(1024),
+    match_threshold float,
+    match_count int
+  )
+  returns table (
+    id bigint,
+    content text,
+    similarity float
+  )
+  language sql stable
+  as $$
+    select
+      trip_docs.id,
+      trip_docs.content,
+      1 - (trip_docs.embedding <=> query_embedding) as similarity
+    from trip_docs
+    where 1 - (trip_docs.embedding <=> query_embedding) > match_threshold
+    order by (trip_docs.embedding <=> query_embedding) asc
+    limit match_count;
+  $$;
